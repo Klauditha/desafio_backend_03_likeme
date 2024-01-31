33ca365d63da0324054a2d71c5d8afe9e289d343
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-30 23:29:26 -0300

    :bug: Correccion en create, se agrega try catch

--- BackEnd/controllers/postsController.js
+++ BackEnd/controllers/postsController.js
@@ -1,18 +1,26 @@
 const { getAllPostsData, addPost } = require('../data/posts.js');
 
 const getAllPosts = async (req, res) => {
-  const results = await getAllPostsData();
-  res.send(results);
+  try {
+    const results = await getAllPostsData();
+    res.send(results);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 const createPost = async (req, res) => {
-  const { titulo, url, descripcion, likes } = req.body;
-  if(!titulo || !url || !descripcion || !likes) {
-    res.status(400).send('Todos los campos son obligatorios');
-    return;
+  try {
+    const { titulo, url, descripcion, likes } = req.body;
+    if (!titulo || !url || !descripcion) {
+      res.status(400).send('Todos los campos son obligatorios');
+      return;
+    }
+    const results = await addPost(titulo, url, descripcion, likes);
+    res.send(results);
+  } catch (error) {
+    res.status(500).send(error);
   }
-  const results = await addPost(titulo, url, descripcion, likes);
-  res.send(results);
 };
 
 module.exports = {
